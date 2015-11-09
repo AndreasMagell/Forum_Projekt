1ab6c333656571235729b9c3304871d4a8e3978d
Andreas Magell <andreas.magell@outlook.com>
2015-11-09 14:44:09 +0100

    Forum entitie. Ej migrerat tll batabasen

---
+++ Forumblank/Forum.Entities/Forum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.Entities
+{
+    class Forum
+    {
+        public int Id { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
