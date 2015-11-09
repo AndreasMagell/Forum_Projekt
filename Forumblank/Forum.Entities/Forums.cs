897f520b90f3d89f8e41a97e00303244bf211884
Andreas Magell <andreas.magell@outlook.com>
2015-11-09 14:53:34 +0100

    ForumDb

--- Forumblank/Forum.Entities/Forums.cs
+++ Forumblank/Forum.Entities/Forums.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Forum.Entities
 {
-    class Forum
+    public class Forums
     {
         public int Id { get; set; }
         public virtual Category Category { get; set; }
---
+++ Forumblank/Forum/DataContexts/ForumDb.cs
@@ -0,0 +1,20 @@
+﻿using Forum.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Forum.DataContexts
+{
+    public class ForumDb : DbContext
+    {
+        public ForumDb()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Forums> Forums { get; set; }
+    }
+}
